1486f94306c3dc244806afe2293fe7e4beacdb26
olivierto <olivier.tosello@gmail.com>
2020-08-26 13:57:17 +0200

    1 file modified > added comments

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
 import { of } from 'rxjs'; 
 import { map,tap, switchMap } from 'rxjs/operators';
 
+//initial observable
 const getQuotesList = of([
   {"id":1,"symbol":"SP-500","date":"1927-12-30T07:00:00.000+00:00","open":17.66,"high":17.66,"low":17.66,"close":17.66,"volume":0},
   {"id":2,"symbol":"SP-500","date":"1928-01-03T07:00:00.000+00:00","open":17.76,"high":17.76,"low":17.76,"close":17.76,"volume":0}
 ])
 
+//just for your interface purpose
 const quotes = getQuotesList
 
 
+/**
+ * function that reduce an array to a {key: [values]} object
+ */
 const groupBy = (array, key) => {
   return array.reduce((result, currentValue) => {
     (result[key] = result[key] || []).push(
@@ -21,9 +26,11 @@ const groupBy = (array, key) => {
 let valuesGroupedByKeys = {}
 
 
+// final observable 
 const allKeyObservable = quotes.pipe(
   switchMap(_quotes => {
     console.log(_quotes)
+    //foreach quotes apply the reducer
     _quotes.forEach((quote)=> {
       Object.keys(quote).forEach((key)=>{
         const result =  groupBy(_quotes,key)
