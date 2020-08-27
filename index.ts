04e36b25e6ca0d01438f44ba335c3c16b710dd4e
olivierto <olivier.tosello@gmail.com>
2020-08-27 10:55:38 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { of } from 'rxjs'; 
 import { map,tap, switchMap } from 'rxjs/operators';
 
-//initial observable
+//initial observable stub of myService.getQuoteList()
 const getQuotesList = of([
   {"id":1,"symbol":"SP-500","date":"1927-12-30T07:00:00.000+00:00","open":17.66,"high":17.66,"low":17.66,"close":17.66,"volume":0},
   {"id":2,"symbol":"SP-500","date":"1928-01-03T07:00:00.000+00:00","open":17.76,"high":17.76,"low":17.76,"close":17.76,"volume":0}
